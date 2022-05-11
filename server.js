0ed2e1a0ea9f2c3d8d4aae7f67904aed2e70963e
Hannah Callison <hannahcallison1007@gmail.com>
2022-05-10 20:17:25 -0700

    debgging package.json

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.set('view engine', 'handlebars');
 app.use("/", allRoutes);
 
 sequelize.sync({ force: false }).then(function() {
-  app.listen(PORT, function() {
+  app.listen(process.env.PORT || 3000, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
