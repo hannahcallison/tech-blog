68b816cef95d73ba02a856926c444a4fd1e3f0eb
Hannah Callison <hannahcallison1007@gmail.com>
2022-05-10 20:10:48 -0700

    debug test

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -15,12 +15,3 @@ document.querySelector("#blogTitle").addEventListener('click', e => {
         // }
     })
 })
-
- 
-const posts = document.querySelectorAll(".blogPost")
-for (const post of posts) {
-    post.addEventListener('click', e => {
-        console.log(e.target.getAttribute('value'))
-        location.href=`/blogpost/${e.target.getAttribute('value')}`;
-    })
-}
